82688cc1b15bc261c051b6fe1beb22f081209417
rbits0 <rbits0@gmail.com>
2024-11-04 04:44:42 +1100

    Make pages static

---
+++ src/routes/+layout.ts
@@ -0,0 +1,5 @@
+export const load = (async () => {
+    return {};
+});
+
+export const prerender = true;
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import { sveltePreprocess, typescript } from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
