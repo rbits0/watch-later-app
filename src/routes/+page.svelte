861a7c26097ee4d154652afd90478fe84b2ff82a
rbits0 <rbits0@gmail.com>
2024-11-04 03:08:13 +1100

    Implement loading of json file and saving to localStorage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,42 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import type { VideoData } from '$lib/VideoData';
   import '../app.scss';
+  
+  const storedVideos = browser ? localStorage.getItem('videos') : null;
+  let videos: VideoData[] = $state(storedVideos ? JSON.parse(storedVideos) : []);
+  let search = $state('');
+
+  $effect(() => {
+    // Save videos
+    if (!browser) {
+      return;
+    }
+
+    localStorage.setItem('videos', JSON.stringify(videos));    
+  })
+  
+
+  function importFile() {
+    const fileInput = document.createElement('input');    
+    fileInput.type = 'file';
+    fileInput.click();
+
+    fileInput.onchange = async () => {
+      if (fileInput.files && fileInput.files.length > 0) {
+        const fileText = await fileInput.files[0].text();
+        addVideos(fileText);
+      }
+    };
+  }
+  
+  function addVideos(newVideos: string | VideoData[]) {
+    if (typeof newVideos === 'string') {
+      newVideos = JSON.parse(newVideos) as VideoData[];
+    }
+    
+    videos = videos.concat(newVideos);
+  }
 </script>
 
 
@@ -10,7 +47,7 @@
 
   <div class='main'>
     <div class='sidebar'>
-      <button>Import File</button>
+      <button onclick={importFile}>Import File</button>
       <button>Set API Key</button>
       <button class='delete-button'>Delete Videos</button>
     </div>
@@ -21,6 +58,7 @@
         type='search'
         placeholder='Search'
         class='search'
+        bind:value={search}
       />
 
       <div class='overflow-auto'>
