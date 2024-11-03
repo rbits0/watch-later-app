23ff3958c491451fb3b5d0ad610e3059b725a22c
rbits0 <rbits0@gmail.com>
2024-11-04 06:24:34 +1100

    Handle escape button in modal

--- src/lib/Modal.svelte
+++ src/lib/Modal.svelte
@@ -10,16 +10,26 @@
   let { showModal = $bindable(), children }: Props = $props();
   
 
+  function handleEscape(event: KeyboardEvent) {
+    if (event.key === 'Escape') {
+      event.preventDefault();
+      showModal = false;
+    }
+  }
+  
+
   if (browser) {
-    const htmlTag = document.querySelector("html")!;
+    const htmlTag = document.querySelector('html')!;
 
     $effect(() => {
       if (showModal) {
         htmlTag.classList.add('modal-is-open');
+        window.addEventListener('keydown', handleEscape);
       }
       
       return () => {
         htmlTag.classList.remove('modal-is-open');
+        window.removeEventListener('keydown', handleEscape);
       }
     });    
   }
