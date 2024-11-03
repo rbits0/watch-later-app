4e2906f89c656967b9a4df00d490377701bb0720
rbits0 <rbits0@gmail.com>
2024-11-04 02:18:19 +1100

    Add main page (non-functional)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,40 @@
 </script>
 
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+  <!-- <DeleteDialog/> -->
+  
+  <!-- <ApiKeyDialog/> -->
+
+  <div class='main'>
+    <div class='sidebar'>
+      <button>Import File</button>
+      <button>Set API Key</button>
+      <button class='delete-button'>Delete Videos</button>
+    </div>
+
+    <div class='main-2'>
+
+      <input
+        type='search'
+        placeholder='Search'
+        class='search'
+      />
+
+      <div class='overflow-auto'>
+
+        <div role='table' class='table'>
+          <div role='row' class='row'>
+            <span role='columnheader' class='cell table-header'>Thumbnail</span>
+            <span role='columnheader' class='cell table-header'>Title</span>
+            <span role='columnheader' class='cell table-header'>Channel</span>
+          </div>
+          
+          <!-- <VideoRow video={video}/> -->
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</div>
---
+++ src/routes/VideoRow.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { VideoData } from "$lib/VideoData";
+
+  interface Props {
+    video: VideoData,
+  }
+  
+  const { video }: Props = $props();
+  const { title, channelName, videoUrl, channelUrl, thumbnailUrls } = video;
+</script>
+
+
+<div role='row' class='row'>
+  <a role='cell' class='cell thumbnail-cell' href={videoUrl}>
+    <img
+      alt='Thumbnail'
+      src={thumbnailUrls ? thumbnailUrls["default"].url : ""}/>
+  </a>
+  <a role='cell' class='cell' href={videoUrl}>
+    {title}
+  </a>
+  <a role='cell' class='cell' href={channelUrl}>
+    {channelName}
+  </a>
+</div>
